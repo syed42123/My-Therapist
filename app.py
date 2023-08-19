47f78167bcc4f90771ae9390753f231b8ffdd2db
syed42123 <142697706+syed42123@users.noreply.github.com>
2023-08-19 19:02:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-
+https://github.com/syed42123/My-Therapist/tree/main
 
 st.set_page_config(page_title="My Online Therapist", layout ="Wide")
 
 # --- Header ---
-
-st.subheader("Hello, I'm Zax :wave:")
-st.title ("Your Personal AI Mental Health Therapist")
-st.write ("In a world where the demands of modern life can often take a toll on our mental well-being. Meet me, Zax—an innovative AI robot meticulously crafted to serve as your personal mental health therapist")
+with st.conatiner():
+  st.subheader("Hello, I'm Zax :wave:")
+  st.title ("Your Personal AI Mental Health Therapist")
+  st.write ("In a world where the demands of modern life can often take a toll on our mental well-being. Meet me, Zax—an innovative AI robot meticulously crafted to serve as your personal mental health therapist")
 
