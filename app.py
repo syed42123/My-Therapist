5aea37cb59b9c88c0e05c63ae7277b49989091d1
syed42123 <142697706+syed42123@users.noreply.github.com>
2023-08-19 19:17:52 -0400

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="My Online Therapist", page_icon=":tada:", layout ="Wide")
+st.set_page_config(page_title="My Online Therapist", page_icon=":tada:", layout ="wide")
 
 # --- Header ---
 with st.conatiner():
