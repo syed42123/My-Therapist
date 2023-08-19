585a88c25604eb86d70790fef152978cefd8c9d9
syed42123 <142697706+syed42123@users.noreply.github.com>
2023-08-19 18:56:54 -0400

    app.py

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ st.set_page_config(page_title="My Online Therapist", layout ="Wide")
 
 # --- Header ---
 
-st.subheader("Hi, I am Zax :wave:")
-st.title 
+st.subheader("Hello, I'm Zax :wave:")
+st.title ("Your Personal AI Mental Health Therapist")
+st.write ("In a world where the demands of modern life can often take a toll on our mental well-being. Meet me, Zax—an innovative AI robot meticulously crafted to serve as your personal mental health therapist")
+
